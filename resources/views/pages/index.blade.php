fdb0ed6b69a762d1be9d7668a47fb20ed3b8d4e7
Daryl De Guzman <ragelavigne@gmail.com>
2019-07-18 21:47:13 -0800

    STYLE: Changed position of online users container to FIXED.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="container" class="container-fluid bg-dark text-light">
     <div class="row justify-content-center pt-5">
-        <div class="col-md-6 mt-5">
+        <div class="col-md-7 mt-5">
             <div class="card bg-dark border-light mb-4">
                 <div class="card-header border-light"><strong>Create Post</strong></div>
                 <div class="card-body border-light">
@@ -73,8 +73,8 @@
             </div>
         </div>
 
-        <div class="col-md-4 mt-5">
-            <div class="card bg-dark border-light">
+        <div class="col-md-3 mt-5">
+            <div class="card bg-dark border-light position-fixed col-md-3">
                 <div class="card-header border-light">Online</div>
                     <p class="text-right p-2 mb-0">Allan Walker</p>
                     <p class="text-right p-2 mb-0">Minda Ryn</p>
