4a8e259eb61c93f994324f90fd6a816ab0f1da33
Daryl De Guzman <ragelavigne@gmail.com>
2019-07-28 03:30:57 -0800

    Created an include for posts. Code for display posts is now on includes. Included the posts file on index.php and on profile timeline.

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,7 +7,7 @@
         <div id="profile-info-container" class="col-12 mt-3">
             <div id="cover-photo" class="row">
                 <div class="p-1 pl-4">
-                    <img id="profile-picture" src="../images/landing-page-img.jpg">
+                    <img class="img-fluid" id="profile-picture" src="../images/landing-page-img.jpg">
                 </div>
                 <div class="p-1">
                     <p id="profile-name" class="p-2">
@@ -136,40 +136,7 @@
             
 
             <div class="row justify-content-left" id="profile-posts">
-                @foreach(App\Post::where('user_id', $user->id)->get() as $post)
-                    <div class="col-12 mb-3">
-                        <div class="card bg-dark border-light p-2">
-                            <div class="card-header border-bottom-0 border-light">
-                                <strong>{{$post->user->fname . ' ' . $post->user->lname}}</strong>
-                                <span id="post-option-{{$post->id}}" class="btn float-right post-options" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <i class="fas fa-ellipsis-h"></i>
-                                    <span class="caret"></span>
-                                </span>
-                                <div class="dropdown-menu dropdown-menu-right bg-dark border-light" aria-labelledby="post-option-{{$post->id}}">
-                                  <span class="dropdown-item btn text-light bg-dark" onclick="showEditPost({{$post->id}})">Edit
-                                  </span>
-                                  <span class="dropdown-item btn text-light bg-dark" onclick="deletePost({{$post->id}})">Delete
-                                  </span>
-                                </div>
-                            </div>
-                            <div class="card-body border-light">
-                                {{$post->body}}
-                            </div>
-                            <div class="card-footer border-light">
-                                <div class="row">
-                                    <div class="col-4 text-center p-0"><i class="fas fa-thumbs-up"></i> Like</div>
-                                    <div class="col-4 text-center p-0"><i class="fas fa-comment-alt"></i> Comment</div>
-                                    <div class="col-4 text-center p-0"><i class="fas fa-share"></i> Share</div>
-                                </div>
-                            </div>
-                            <div class="card-footer border-light">
-                                <input class="form-control bg-dark" type="text" name="comment" placeholder="Type a comment...">
-                            </div>
-                        </div>
-                    </div> 
-                @endforeach
-
-                
+                @include('includes.posts')
             </div>
         </div>
     </div>
