8c77f42cb156ded315602fd48f40d02a71fc2764
Daryl De Guzman <ragelavigne@gmail.com>
2019-09-03 17:17:12 -0800

    Created relationship for Conversation and Message

--- app/Conversation.php
+++ app/Conversation.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    public function messages(){
+    	return $this->hasMany(Message::class, 'conversation_id', 'conversation_id');
+    }
 }
