aef110d7e47855d0a374822067c2c95bcf9b4022
Daryl De Guzman <ragelavigne@gmail.com>
2019-09-06 14:54:32 -0800

    Created relationship for contact_id

--- app/Conversation.php
+++ app/Conversation.php
@@ -9,4 +9,8 @@ class Conversation extends Model
     public function messages(){
     	return $this->hasMany(Message::class, 'conversation_id', 'conversation_id');
     }
+
+    public function contact(){
+    	return $this->hasOne(User::class, 'id', 'contact_id');
+    }
 }
