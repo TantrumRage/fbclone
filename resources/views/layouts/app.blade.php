e3777e65c962978163c030499be4ebb7a470d0b2
Daryl De Guzman <ragelavigne@gmail.com>
2019-09-13 17:10:09 -0800

    Set scripts to defer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,11 +17,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css">
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-
 </head>
 <body>
     <div id="app">
@@ -36,6 +31,10 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
 
     <!-- Scripts with Global variables -->
     <script>
