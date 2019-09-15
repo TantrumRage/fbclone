b09e0a7c0b6079ecb5c1b5c12ed5faea56c1c34a
Daryl De Guzman <ragelavigne@gmail.com>
2019-09-14 21:06:52 -0800

    Redirect to '/' instead of '/home' after login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/main.js') }}" defer></script>
+    <script src="{{ asset('js/main.js') }}" defer=""></script>
 
     <!-- Scripts with Global variables -->
     <script>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,7 +14,7 @@
 
         <div class="col-md-3 mt-5">
             <div class="card bg-dark-gray border-light-gray position-fixed col-md-3">
-                <div class="card-header border-light-gray">Online</div>
+                <div class="card-header border-light-gray">Online ( <span id="online-counter"></span> )</div>
                     <p class="text-right p-2 mb-0">Allan Walker</p>
                     <p class="text-right p-2 mb-0">Minda Ryn</p>
                 <div class="card-body border-light-gray">
